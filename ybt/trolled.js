1f5af2b00b3992ae5d14e4a87e4f9eef7f3be2b3
GamingGamer521 <134823809+gm521@users.noreply.github.com>
2024-07-25 11:49:46 -0400

    Update trolled.js

--- ybt/trolled.js
+++ ybt/trolled.js
@@ -6,7 +6,7 @@ c = '$a = New-Object System.Media.SoundPlayer;' +
     '    $f = New-Object system.Windows.Forms.Form;' +
     '    $f.StartPosition = \'manual\';' +
     '    $f.Top = (Get-Random -Minimum 0 -Maximum 1000);' +
-    '    $f.Left = (Get-Random -Minimum 0 -Maximum 1000);' +
+    '    $f.Left = (Get-Random -Minimum 0 -Maximum 2000);' +
     '    $f.Width = 350;' +
     '    $f.Height = 310;' +
     '    $f.FormBorderStyle = \'none\';' +
